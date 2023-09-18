8e58b900a266ecceffe2cb313574d985f9f83954
nojram00 <113487670+nojram00@users.noreply.github.com>
2023-09-18 11:51:07 +0800

    lolz

---
+++ src/app/api/items/[category]/[compatibility]/route.ts
@@ -0,0 +1,20 @@
+import { NextRequest, NextResponse } from "next/server";
+import clientCon from "../../../../../../config/mongodb.config";
+import headers from "../../../../../../lib/apiHeader";
+
+export async function GET(req : NextRequest, {params} : {params: {category: string, compatibility: string}}){
+
+    const client = await clientCon
+
+    const db = client.db('hub_new_db')
+
+
+    console.log(params.category)
+    const items = await db.collection('items')
+                        .find({"data.category" : params.category, "data.compatibility" : params.compatibility})
+                        .toArray()
+
+    // console.log(items)
+
+    return NextResponse.json(items, {headers});
+}
